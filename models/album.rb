52281b3634f5e22e3281dff42d85e1d77ecdd483
Donald Cameron <user@Users-MBP-5.lan>
2020-05-20 16:18:33 +0100

    Initial commit, DB created, mostly working

---
+++ models/album.rb
@@ -0,0 +1,84 @@
+require('pg')
+require_relative('../db/sql_runner')
+
+class Album
+
+  attr_accessor :title, :genre, :released, :artist_id
+  attr_reader :id
+
+  def initialize(options)
+    @title = options['title']
+    @genre = options['genre']
+    @released = options['options'].to_i
+    @artist_id = options['artist_id'].to_i
+    @id = options['id'].to_i if options['id']
+  end
+
+  def artist()
+    sql = "SELECT * FROM artists WHERE id = $1"
+    values = [@artist_id]
+    results = SqlRunner.run(sql, values)
+    artist_hash = results[0]
+    artist = Artist.new(artist_hash)
+    return artist
+  end
+
+  def save()
+    sql = "INSERT INTO albums
+    (
+      title,
+      genre,
+      released,
+      artist_id
+    ) VALUES
+    (
+      $1, $2, $3, $4
+    )
+    RETURNING id"
+    values = [@title, @genre, @released, @artist_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def update()
+    sql = "
+    UPDATE albums SET (
+      title,
+      genre,
+      released,
+      artist_id
+    ) =
+    (
+      $1,$2, $3, $4
+    )
+    WHERE id = $5"
+    values = [@title, @genre, @released, @artist_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM albums where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM albums WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    album_hash = result.first
+    album = Album.new(album_hash)
+    return album
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM albums"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM albums"
+    pg_results = SqlRunner.run(sql)
+    return albums.map { |album| Album.new(album) }
+  end
+
+end
---
+++ models/artist.rb
@@ -0,0 +1,66 @@
+require('pg')
+require_relative('../db/sql_runner.rb')
+require_relative('./album.rb')
+
+class Artist
+
+attr_reader :id
+attr_accessor :name
+
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @name = options['name']
+end
+
+def album()
+  sql = "SELECT * FROM albums
+  WHERE artist_id = $1"
+  values = [@id]
+  album_hashes = SqlRunner.run(sql, values)
+  album_objects = album_hashes.map { |album_hash| Album.new(album_hash) }
+  return album_objects
+end
+
+def save()
+  sql = "INSERT INTO artists (name) VALUES ($1)
+  RETURNING id"
+  values = [@name]
+  @id = SqlRunner.run(sql, values)[0]['id'].to_i
+end
+
+def update()
+  sql = "
+  UPDATE artists SET (name) = ($1) WHERE id = $2"
+  values = [@name, @id]
+  SqlRunner.run(sql, values)
+end
+
+def delete()
+  sql = "DELETE FROM artists where id = $1"
+  values = [@id]
+  SqlRunner.run(sql, values)
+end
+
+def self.find(id)
+  sql = "SELECT * FROM artists WHERE id = $1"
+  values = [id]
+  db.prepare("find", sql)
+  results = SqlRunner.run(sql, values)
+  db.close()
+  artist_hash = results.first
+  artist = Customer.new(artist_hash)
+  return artist
+end
+
+def self.delete_all()
+  sql = "DELETE FROM artists"
+  SqlRunner.run(sql)
+end
+
+def self.all()
+  sql = "SELECT * FROM artists"
+  artists = SqlRunner.run(sql)
+  return artists.map {|artist| Customer.new(artist)}
+end
+
+end
